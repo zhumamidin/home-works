a038dc33ce3d3e462e8ad706013570b435f79709
Zhumamidin Zholdoshov <zhumamidin.zholdoshov@gmail.com>
2021-07-04 09:58:45 +0300

    Закончил 1-ю и 2-ю домашку по JS

---
+++ Home_work_26/js/main.js
@@ -0,0 +1,43 @@
+// // The first example
+
+// alert("Привет");
+// alert("Мир");
+
+// // The second example
+
+// let message;
+// message = "hello";
+// alert(message);
+
+// // The third example
+
+// let user = "John";
+// let age = "25";
+// let message = "hello";
+
+// // The fourth example
+
+// let message = "Один";
+// let message = "Другое";
+// alert("message");
+
+// // The fifth example
+
+// let hi = "hello";
+// alert(hi);
+
+// const myBirthDay = "27.02.1998";
+// alert(myBirthDay);
+
+// const COLOR_WHITE = "#ffffff";
+// const COLOR_BLACK = "#000";
+// alert(COLOR_WHITE);
+
+let name = "Ivan";
+let admin = name;
+alert(admin);
+
+let OurPlanet = "Earth";
+let currentUserName = "John";
+alert(OurPlanet);
+alert(currentUserName);
