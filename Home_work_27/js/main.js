1729f4389dabadca8604fef3bf75457e54561041
Zhumamidin Zholdoshov <zhumamidin.zholdoshov@gmail.com>
2021-07-09 18:08:07 +0300

    Типы данных и базовые операторы.

---
+++ Home_work_27/js/main.js
@@ -0,0 +1,192 @@
+/*// let message = "Ivan";
+// message = "12345";
+// alert(message);
+
+// alert(1 / 0);
+
+// alert("Ivan" / 2);
+
+/////////////////////////////////
+
+// let admin;
+// let name;
+
+// name = "Asan";
+// admin = name;
+
+// alert(admin);
+
+// let admin;
+// admin = "Uson";
+
+/////////////////////////////
+
+// console.log(admin);
+
+// console.log("Asan");
+
+// console.error("MyError");
+
+// console.warn("My custom warning");
+
+///////////////////////////////////////
+
+let MyNumber = 10;
+MyNumber = -10.7878;
+MyNumber = 10.5;
+MyNumber = "Ivan";
+
+console.log(MyNumber);
+
+// Infinity
+////////////////////////////
+
+console.log(3 / 0);
+
+//NaN
+//////////////////////////////
+
+console.log("Ivan" / 2);
+
+//BigInt
+/////////////////////////////////////
+let myBigNum = 238432478323894739874487324738243n;
+
+console.log(myBigNum);
+
+//Strings
+//////////////////////////////////
+
+let myString = "I don't know";
+myString = 'Иван сказал "Я хороший"';
+
+//Экранирование элементов
+myString = 'Иван сказал "Я хороший" \' \\';
+
+console.log(myString);
+
+//Template Strings
+/////////////////////////////
+
+let userName = "John";
+let message = `Hello, ${userName}!`;
+
+console.log(message);
+
+//BooLean
+////////////////////////////////
+
+let user = "John";
+let userAge = 42;
+let isMarried = false;
+
+//Null
+////////////////////////////////
+
+let age = null;
+age = 25;
+
+*/
+
+/*Undefined
+
+let myString = "Ivan";
+let myNumber = 500;
+let myNumber2 = 500.1;
+let myBigNum = 8584377563478734583465387n;
+let age = null;
+let userName;
+
+console.log(typeof userName);*/
+
+//Operator
+/////////////////////////
+
+/*
+
+let a = 5;
+let b = 5;
+
+//+
+
+// let result = a + b;
+
+//-
+// let result = a - b;
+
+//*
+// let result = a * b;
+
+// /
+// let result = a / b;
+
+// %
+// let result = a % b;
+
+// **
+// let result = a ** b;
+
+console.log(result);
+*/
+
+/*
+//Унарные
+//+
+
+let apples = "2";
+let oranges = "3";
+
+apples = +apples;
+oranges = +oranges;
+
+let result = apples + oranges;
+
+console.log(result);
+*/
+
+// console.log((2 + 2) * 5);
+
+//Increment
+
+/*let counter = 1;
+counter++;
+
+console.log(counter);
+*/
+
+/*
+//Math
+
+const pi = Math.PI;
+
+let a = Math.round(pi);
+let b = Math.ceil(pi);
+let c = Math.floor(pi);
+
+console.log(pi);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(pi.toFixed(2));
+
+
+// method Random
+///////////////////////////
+
+let a = Math.random();
+a = Math.ceil(Math.random() * 5);
+
+console.log(a);
+
+*/
+
+//HomeWork
+///////////////////////////
+
+let name = "Ilya";
+
+alert(`hello ${1}`); // ?
+
+alert(`hello ${"name"}`); // ?
+
+alert(`hello ${name}`); // ?
